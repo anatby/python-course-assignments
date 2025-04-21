6c9f78f3760c6d1a05117df20d8dc04544b17772
anatby <anatby@gmail.com>
2025-04-21 15:54:30 +0300

    correct the code

--- day03/circle.py
+++ day03/circle.py
@@ -1,10 +1,7 @@
 import math    
 import sys
-radius = int(input("Enter the radius of the circle: "))
-area = math.pi * radius ** 2
-circumference = 2 * math.pi * radius
-print(f"Area of the circle: {area:.2f}")
-print(f"Circumference of the circle: {circumference:.2f}")
+
+
 
 if len(sys.argv) != 2:
     print("Usage: python circle.py <radius>")
@@ -12,6 +9,10 @@ if len(sys.argv) != 2:
 
 try:
     radius = float(sys.argv[1])
+    area = math.pi * radius ** 2    
+    circumference = 2 * math.pi * radius
+    print(f"Area of the circle: {area:.2f}")
+    print(f"Circumference of the circle: {circumference:.2f}")
 except ValueError:
     print("Please provide a valid number for the radius.")
     sys.exit(1)
